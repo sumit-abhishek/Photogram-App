45628c1c7441651d20614e98b988a90f11eb3eab
Sumit Abhishek <sumitabhi1997@gmail.com>
2025-03-20 22:41:30 +0530

    Created Context API for the Project

---
+++ src/context/userAuthContext.tsx
@@ -0,0 +1,79 @@
+import { auth } from "@/firebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  User,
+} from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface IUserAuthProvider {
+  children: React.ReactNode;
+}
+
+type AuthContextData = {
+  user: User | null;
+  logIn: typeof logIn;
+  signUp: typeof signUp;
+  logOut: typeof logOut;
+  googleSignIn: typeof googleSignIn;
+};
+
+const logIn = (email: string, password: string) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+const signUp = (email: string, password: string) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+const logOut = () => {
+  signOut(auth);
+};
+
+const googleSignIn = () => {
+  const googleAuthProvider = new GoogleAuthProvider();
+  return signInWithPopup(auth, googleAuthProvider);
+};
+
+export const userAuthContext = createContext<AuthContextData>({
+  user: null,
+  logIn,
+  signUp,
+  logOut,
+  googleSignIn,
+});
+
+export const userAuthProvider: React.FunctionComponent<IUserAuthProvider> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("The logged user in state is:", user);
+        setUser(user);
+      }
+      return () => unsubscribe();
+    });
+  });
+  const value: AuthContextData = {
+    user,
+    logIn,
+    signUp,
+    logOut,
+    googleSignIn,
+  };
+  return (
+    <userAuthContext.Provider value={value}>
+      {children}
+    </userAuthContext.Provider>
+  );
+};
+
+export const useUserAuth = () => {
+  return useContext(userAuthContext);
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <ProtectedRoutes />,
     children: [
       {
-        path: "/home",
+        path: "/",
         element: <Home />,
         errorElement: <Error />,
       },
